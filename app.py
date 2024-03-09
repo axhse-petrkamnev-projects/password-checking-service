890c781bd648fd819772e0209ebe4c26ea33c895
Petr Kamnev <petrkamnevyt@gmail.com>
2024-03-09 21:23:40 +0300

    resource_dir parameter support

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
-import requests
 from flask import Flask, render_template
 from storage.pwned_storage import PwnedStorage
+import os
 
-storage = PwnedStorage("/tmp/pwned-storage")
-app = Flask(__name__, template_folder="templates")
+def create_app():
+    app = Flask(__name__, template_folder="templates")
 
-@app.route('/')
-def home():
-    return render_template('client-page.html')
+    storage_path = os.getenv('RESOURCE_DIR', '/tmp/pwned-storage')
+    storage = PwnedStorage(storage_path)
 
-@app.route("/range/<prefix>")
-def prefix_search(prefix):
-    try:
-        response = storage.get_range(prefix)
-        return response, 200, {"Content-Type": "text/plain"}
-    except:
-        return "Bad prefix", 400, {"Content-Type": "text/plain"}
+    @app.route('/')
+    def home():
+        return render_template('client-page.html')
+
+    @app.route("/range/<prefix>")
+    def prefix_search(prefix):
+        try:
+            response = storage.get_range(prefix)
+            return response, 200, {"Content-Type": "text/plain"}
+        except:
+            return "Bad prefix", 400, {"Content-Type": "text/plain"}
+
+    return app
 
 if __name__ == "__main__":
+    app = create_app()
     app.run()
